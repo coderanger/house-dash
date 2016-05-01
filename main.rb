defb76a6205ed1554f1fc65f544052c80075d3c3
Noah Kantrowitz <noah@coderanger.net>
2016-04-30 20:11:35 -0700

    Remove some unused bits.

--- main.rb
+++
@@ -1,9 +0,0 @@
-require 'rest-client'
-require 'rss'
-require 'sinatra'
-
-get '/' do
-  # News ticker data.
-  @bbc = RSS::Parser.parse(RestClient.get('http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml'))
-  erb :index
-end
